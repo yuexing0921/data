3a6cfc929881f73ca9df17526ccc9eb1f674188b
yiquanwen <yiquanwen@xiaoheiban.cn>
2020-06-17 20:12:48 +0800

    chore: auto publish by invest

---
+++ precache-manifest.e63da64755b916b348f69055dd21356e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be80ce2d55b20e2f8d1a87cd09b0a3f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4b58f5638f2b9d4f046",
+    "url": "./static/css/2.b218983f.chunk.css"
+  },
+  {
+    "revision": "0dcc887378480a87d492",
+    "url": "./static/css/main.d04338ce.chunk.css"
+  },
+  {
+    "revision": "f4b58f5638f2b9d4f046",
+    "url": "./static/js/2.46088ee7.chunk.js"
+  },
+  {
+    "revision": "fb8b26dc795e1f56d6f99cb84f61ae46",
+    "url": "./static/js/2.46088ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dcc887378480a87d492",
+    "url": "./static/js/main.5530281c.chunk.js"
+  },
+  {
+    "revision": "ba0ac4b1ad6d4198eda1",
+    "url": "./static/js/runtime-main.779340af.js"
+  }
+]);
